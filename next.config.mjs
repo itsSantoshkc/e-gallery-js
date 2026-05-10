70578b0b47790f0216034cd964232ebfd3579d43
Santosh K.C. <santoshkc7786@gmail.com>
2026-05-10 18:57:56 +0545

    Next Config Changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: {
-      protocol: "https",
-      hostname: "res.cloudinary.com",
-    },
+    remotePatterns: [new URL("https://res.cloudinary.com")],
   },
 };
 
