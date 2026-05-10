aae1a28255f4a74bb0ddc62a24504450993d0688
Santosh K.C. <santoshkc7786@gmail.com>
2026-05-10 19:00:18 +0545

    Updated remote patterns value to [Object] from just Object

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [new URL("https://res.cloudinary.com")],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
   },
 };
 
