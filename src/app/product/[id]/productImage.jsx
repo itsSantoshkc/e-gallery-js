90ddd4698b6f774d9c6540a44cce8c7375b2787b
Santosh K.C. <santoshkc7786@gmail.com>
2026-05-10 19:18:02 +0545

    Fixed Product Image Issue

--- src/app/product/[id]/productImage.jsx
+++ src/app/product/[id]/productImage.jsx
@@ -2,7 +2,16 @@ import Image from "next/image";
 import React from "react";
 
 const productImage = ({ image, name }) => {
-  return <Image src={image} alt={name} fill className="object-contain" />;
+  return (
+    <Image
+      src={image}
+      alt={name}
+      width={0}
+      height={0}
+      sizes="100vw"
+      className="object-cover w-full h-full"
+    />
+  );
 };
 
 export default productImage;
